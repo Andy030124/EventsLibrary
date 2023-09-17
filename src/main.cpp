4f17e9111a93a4750196f7d9b61c149aaf920407
Kirlen <garciandy579@gmail.com>
2023-09-16 22:37:36 -0400

    Example of correct usage.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include "Events.hpp"
 #include <iostream>
 #include <chrono>
-#include <thread>
 
 int main(int argc, char const *argv[]){
     using namespace std::chrono_literals;
 
-    events::EventEmitter mevents{};
+    events::EventEmitter mevents{}; // main object
 
+	// create event "/start"
     mevents.on("/start", [](){
         std::cout << "Hola Mundo! \n";
     });
 
+	// to pass an argument to lambda we need specified type on template
     mevents.on<int>("/help", [](int a){
         std::cout << "Ayuda: " << a << "\n";
     });
@@ -21,24 +22,29 @@ int main(int argc, char const *argv[]){
         std::cout << "Suma de " << a << " + " << b << " = " << res << "\n";
     });
 
+	// emit a work to time specified and arguments for pass
     mevents.emitWork("/help", 3s, 10);
 
+	// instant emit 
     mevents.emit("/start");
+    // instant emit with param
     mevents.emit("/help", 5);
-
+	// disable event
     mevents.undo("/start");
-    auto& func = mevents.get<int>("/help");
+    auto& func = mevents.get<int>("/help"); // get a callback associate with a event
     func(5);
 
     mevents.emit("/start");
     mevents.emit("/help", 2);
 
+	// support reference
     int mresult = 0;
 
     mevents.emit("/sum", 5, 2, mresult);
 
     std::cout << "Resultado de suma: " << mresult << std::endl;
 
+	// is good practice always on the final wait for all works
     mevents.waitWorks();
 
     return 0;
